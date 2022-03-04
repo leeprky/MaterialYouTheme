5d85b03fda28db2df212bbadc4b9226fb4c3a6dd
Lee <71296232+leeprky@users.noreply.github.com>
2022-03-04 19:10:11 +0000

    0.9.4
    - MAJOR OPTIMISATION
    - 5 NEW THEMES (Total 12)
    - ADDITIONAL THEMING

---
+++ src/scheme/_material-user-preset.scss
@@ -0,0 +1,147 @@
+/*
+ __  __  ___  _____  ___  ___  ___  ___  _          __   __  ___   _   _        ___  ___  ___  ___  ___  _____ 
+|  \/  |/   \|_   _|| __|| _ \|_ _|/   \| |         \ \ / / / _ \ | | | |      | _ \| _ \| __|/ __|| __||_   _|
+| |\/| || - |  | |  | _| |   / | | | - || |__        \   / | (_) || |_| |      |  _/|   /| _| \__ \| _|   | |  
+|_|  |_||_|_|  |_|  |___||_|_\|___||_|_||____|        |_|   \___/  \___/       |_|  |_|_\|___||___/|___|  |_|  
+                                      https://textfancy.com/ascii-art/
+*/
+
+@import "";
+
+/* ^^ Any Other Additional Imports Above ^^ */
+
+/* Colours */
+/* Use A Editor Like Visual Studio Code For Ease Of Use */
+/* Or Refer To Default Colours At Bottom */
+
+:root {
+   --style-name: "Material You Preset Theme";
+   --style-creator: "Leeprky#2063";
+  /* Any Custom --variables Below */
+  
+  --example: "...";
+}
+
+.theme-dark {
+  --main-colour: 0, 0, 0 !important; /* The Blurple Replacement */
+  --main-colour2: #000000 !important; 
+  --main-colour3: 000 !important; 
+  --main-background: #000000 !important; /* Topbar */
+  --secondary-background: #000000 !important; /* Most Things */
+  --tertiary: #000000 !important; /* Not Used Mostly Set as main background */
+  --floating: #000000 !important; /* Dropdowns */
+
+  --headers: #000000 !important; 
+  --headers-secondary: #000000 !important;
+  
+  --namesusernames: #000000 !important;
+  --text: #000000 !important;
+  --text-positive1: var(--green-color) !important;
+  --muted: #000000 !important; /* I.E: Icons */
+  --links: var(--alt-color2) !important;
+  --button-interactive: #000000 !important;
+  --button-interactive-hover: var(--main-colour2) !important;
+  --button-interactive-active: #000000 !important;
+  --button-interactive-muted: #000000 !important;
+}
+.theme-light {
+  --main-colour: 255, 255, 255 !important; /* The Blurple Replacement */
+  --main-colour2: #FFFFFF !important; 
+  --main-colour3: 255 !important; 
+  --main-background: #FFFFFF !important; /* Topbar */
+  --secondary-background: #FFFFFF !important; /* Most Things */
+  --tertiary: #FFFFFF !important; /* Not Used Mostly Set as main background */
+  --floating: #FFFFFF !important; /* Dropdowns */
+
+  --headers: #FFFFFF !important; 
+  --headers-secondary: #FFFFFF !important;
+  
+  --namesusernames: #FFFFFF !important;
+  --text: #FFFFFF !important;
+  --text-positive1: var(--green-color) !important;
+  --muted: #FFFFFF !important; /* I.E: Icons */
+  --links: var(--alt-color2) !important;
+  --button-interactive: #FFFFFF !important;
+  --button-interactive-hover: var(--main-colour2) !important;
+  --button-interactive-active: #FFFFFF !important;
+  --button-interactive-muted: #FFFFFF !important;
+}
+
+/* Any Extra Code Below */
+
+.example123 {
+    background-color: #0000;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* IGNORE BELOW */
+/* DEFAULT COLOURS --- MATERIAL BLUE
+.theme-dark {
+  --main-colour: 90, 130, 179 !important;
+  --main-colour2: #5a82b3 !important; 
+  --main-colour3: 213 !important; 
+  --main-background: #152029 !important;
+  --secondary-background: #111a22 !important;
+  --tertiary: #000000 !important;
+  --floating: #0e151c !important;
+  --headers: #b2cef0 !important; 
+  --headers-secondary: #a9caf2 !important;
+  --namesusernames: #d1e0f2 !important;
+  --text: #d1e0f2 !important;
+  --text-positive1: var(--green-color) !important;
+  --muted: #d1e0f2 !important;
+  --links: var(--alt-color2) !important;
+  --button-interactive: #cbdcf2 !important;
+  --button-interactive-hover: var(--main-colour2) !important;
+  --button-interactive-active: #b2cef0 !important;
+  --button-interactive-muted: #b2cef0 !important;
+}
+.theme-light {
+  --main-colour: 113, 164, 226 !important; 
+  --main-colour2: #71a4e2 !important; 
+  --main-colour3: 213 !important; 
+  --main-background: #ebf3fc !important;
+  --secondary-background: rgb(219, 236, 255) !important;
+  --tertiary: #ffffff !important;
+  --floating: #ffffff !important;
+  --headers: #343c46 !important; 
+  --headers-secondary: #1c2127 !important;
+  --namesusernames: #272a2e !important;
+  --text: #1e2022 !important;
+  --text-positive1: var(--green-color) !important;
+  --muted: #18191b !important;
+  --links: var(--alt-color2) !important;
+  --button-interactive: #15171a !important;
+  --button-interactive-hover: var(--main-colour2) !important;
+  --button-interactive-active: #15191d !important;
+  --button-interactive-muted: #13161a !important;
+}
+*/
